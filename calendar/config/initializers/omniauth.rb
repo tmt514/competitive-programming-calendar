8aa605568b51761a617a8412f35420b080190790
tmt514 <math.tmt514@gmail.com>
2015-12-28 08:19:56 +0000

    target new window

--- calendar/config/initializers/omniauth.rb
+++ calendar/config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, callback_url: "http://blog.icpc.tw/auth/facebook/callback"
+  if ENV['RAILS_ENV'] == 'production'
+    provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, callback_url: "http://blog.icpc.tw/auth/facebook/callback"
+  else
+    provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret
+  end
 end
