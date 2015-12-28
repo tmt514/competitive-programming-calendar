9f2358a7a90e245f1fb4e4a385f1e663137676b3
tmt514 <math.tmt514@gmail.com>
2015-12-28 06:54:54 +0000

    omniauth

--- calendar/config/initializers/omniauth.rb
+++ calendar/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, callback_path: "http://blog.icpc.tw/auth/facebook/callback"
+  provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, callback_url: "http://blog.icpc.tw/auth/facebook/callback"
 end
