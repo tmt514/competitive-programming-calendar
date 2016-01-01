b09d71207082ba509c71c6e5e5d651a6d5f1dac1
tmt514 <math.tmt514@gmail.com>
2016-01-01 16:23:44 +0000

    change default host name

--- calendar/app/controllers/application_controller.rb
+++ calendar/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def default_url_options
+    if Rails.env.production?
+      { host: 'blog.icpc.tw' }
+    else
+      {}
+    end
+  end
 end
